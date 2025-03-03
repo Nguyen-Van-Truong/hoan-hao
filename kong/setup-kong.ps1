c96a39c5b418aea51c38338b80c2a9ab56117c69
Nguyen-Van-Truong <115326716+Nguyen-Van-Truong@users.noreply.github.com>
2025-03-03 16:49:08 +0700

    setup postService in Kong

--- kong/setup-kong.ps1
+++ kong/setup-kong.ps1
@@ -23,28 +23,54 @@ Invoke-RestMethod -Uri "http://localhost:8001/services" -Method Post -Body @{
     url  = "http://host.docker.internal:8081"
 } -ContentType "application/x-www-form-urlencoded"
 
+Write-Host "Registering PostService..."
+Invoke-RestMethod -Uri "http://localhost:8001/services" -Method Post -Body @{
+    name = "post-service"
+    url  = "http://host.docker.internal:8082"
+} -ContentType "application/x-www-form-urlencoded"
+
 # Bước 3: Đăng ký Routes
 Write-Host "Adding route for AuthService..."
 Invoke-RestMethod -Uri "http://localhost:8001/services/auth-service/routes" -Method Post -Body @{
-    "paths[]"    = "/auth"
-    name         = "auth-route"
-    strip_path   = "false"  # Sửa từ "true" thành "false"
+    "paths[]"   = "/auth"
+    name        = "auth-route"
+    strip_path  = "false"
 } -ContentType "application/x-www-form-urlencoded"
 
 Write-Host "Adding route for UserService..."
 Invoke-RestMethod -Uri "http://localhost:8001/services/user-service/routes" -Method Post -Body @{
-    "paths[]" = "/user"
-    name      = "user-route"
-    strip_path = "false"  # Sửa từ "true" thành "false"
+    "paths[]"   = "/user"
+    name        = "user-route"
+    strip_path  = "false"
 } -ContentType "application/x-www-form-urlencoded"
 
-# Bước 4: Kích hoạt JWT Plugin cho UserService
+Write-Host "Adding route for PostService..."
+Invoke-RestMethod -Uri "http://localhost:8001/services/post-service/routes" -Method Post -Body @{
+    "paths[]"   = "/post"
+    name        = "post-route"
+    strip_path  = "false"
+} -ContentType "application/x-www-form-urlencoded"
+
+Write-Host "Adding route for Comment in PostService..."
+Invoke-RestMethod -Uri "http://localhost:8001/services/post-service/routes" -Method Post -Body @{
+    "paths[]"   = "/comment"
+    name        = "comment-route"
+    strip_path  = "false"
+} -ContentType "application/x-www-form-urlencoded"
+
+# Bước 4: Kích hoạt JWT Plugin
 Write-Host "Enabling JWT plugin for UserService..."
 Invoke-RestMethod -Uri "http://localhost:8001/services/user-service/plugins" -Method Post -Body @{
     name = "jwt"
 } -ContentType "application/x-www-form-urlencoded"
 
-# Bước 5: Tạo Consumer và JWT Credential
+Write-Host "Enabling JWT plugin for PostService (post routes)..."
+Invoke-RestMethod -Uri "http://localhost:8001/services/post-service/plugins" -Method Post -Body @{
+    name        = "jwt"
+    "config.uri_param_names" = "jwt" # Tùy chọn nếu cần thêm vào URI
+} -ContentType "application/x-www-form-urlencoded"
+
+# Bước 5: Tạo Consumer và JWT Credential (giữ nguyên cho test-user)
 Write-Host "Creating consumer 'test-user'..."
 Invoke-RestMethod -Uri "http://localhost:8001/consumers" -Method Post -Body @{
     username = "test-user"
